aed62fe3469927eea5f7056843f1a63b3c958816
ankm24 <ankm174@gmail.com>
2024-04-19 15:56:47 +0900

    시작 파일 수정

--- cat Project/cat Project/시작.c
+++ cat Project/cat Project/시작.c
@@ -3,57 +3,55 @@
 #include <string.h>
 #include <stdlib.h>
 #include <conio.h>
+//#include<catFunc.h>
 
-#pragma once
-
-#define pause system("pause > nul")		// DEFINE 선언
-#define cls system("cls")
-
-#define UP 0
-#define DOWN 1
-#define SUBMIT 2
-
-void SetColor(int color){
-	SetConsoleTextAttribute(GetSTDHandle(STD_OUTPUT_HANDLE), color);
+//#pragma once
+//
+//#define pause system("pause > nul")		// DEFINE 선언
+//
+//#define UP 0
+//#define DOWN 1
+//#define SUBMIT 2
+//
+void setconsleview() {
+	system("mode con:cols=80 lines=30");
 }
-void CursorView(char show) {
-	HANDLE hConsole;
-	CONSOLE_CURSOR_INFO ConsoleCursor;
-
-	hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-
-	ConsoleCursor.bVisible = show;
-	ConsoleCursor.dwSize = 1;
-
-	SetConsoleCursorInfo(hConsole, &ConsoleCursor);
-}
-void gotoxy(int x, int y) {
-	COORD pos = { x,y };
-	SetConsoleCursorPosition(GetSTDHandle(STD_OUTPUT_HANDLE), pos);
-}
-void mainPtr(void);
-
-int main(void) {
-	CursorView(0);
-	system("COLOR OF");
-	mainPtr;
-	pause;
-	return 0;
-}
-int drawMenu() {
-	int x = 15, y = 20;
-	printf("\n\n\n");
-	printf("/////////////////////////////////////////////////////////");
-	printf("//													   //");
-	printf("//	 _   _  _ _|_  _ _ _|_  _   _ _   .  _  _ _|_	   //");
-	printf("//	_\VV(/_(/_ |  (_(_| |  |_) | (_)  | (/_(_  |	   //");
-	printf("//						   |		 L|				   //");
-	printf("//													   //");
-	printf("//
-	printf("//
-	printf("//							
-	printf("//							
-
-
-
+//
+//int main(void) {
+//	CursorView(0);	// 커서의 깜빡임을 숨겨준다
+//	system("COLOR OF");	// 화면 색깔을 검정, 글씨 색깔을 하얀색으로 설정
+//	//mainPtr;
+//	pause;
+//	return 0;
+//}
+int main() {
+	setconsleview();
+	
+	printf("■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□\n");
+	printf("□                                                                            ■\n");
+	printf("■                 _   _  _ _|_   _ _ _|_   _  _ _  . _  __|_                 □\n");
+	printf("□                 _\VV(/_(/_ |   (_(_| |   |_)| (_) |(/_(_ |           '      ■\n");
+	printf("■                                         |       L|                         □\n");
+	printf("□    '      *         *          ,MMM8&&&&.                     *            ■\n");
+	printf("■                          *     MMM88&&&&&                                  □\n");
+	printf("□                                MMMM88&&&&    .    *                        ■\n");
+	printf("■      *                         MM88&&&&&&                                  □\n");
+	printf("□                   *            'MM88&&&&'                   *              ■\n");
+	printf("■            '                         *                                *    □\n");
+	printf("□              *       /|/|_      __/||      *                               ■\n");
+	printf("■                     /    -|    /-  ~|  .             *                     □\n");
+	printf("□                    |    = Y = T_ = /                                       ■\n");
+	printf("■        '       *    )==*(`     `) ~(                             *         □\n");
+	printf("□  *                 /     |     /    |          *                           ■\n");
+	printf("■                   |      |     ) ~  (                                      □\n");
+	printf("□                   /      |    /    ~|                    ---------------   ■\n");
+	printf("■             *     |      /    |~   ~/                   |    시작하기   |  □\n");
+	printf("□    ___________/|_/|__  _/_/|_/|__~__/_/|_/|_/|_/|_/|_    ---------------   ■\n");
+	printf("■    |  |  |  |  |  | ) ) |  |  | ((  |  |  |  |  |  |     ---------------   □\n");
+	printf("□    |  |  |  |  |  | )_) |  |  |  )) |  |  |  |  |  |    |    게임 종료  |  ■\n");
+	printf("■    |  |  |  |  |  |  |  |  |  | (/  |  |  |  |  |  |     ---------------   □\n");
+	printf("□                                                                            ■\n");
+	printf("■                                                                            □\n");
+	printf("□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■\n");
+	
 }

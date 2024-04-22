aca74241c0ceb3315426bf870b67468037cd5f04
ankm24 <ankm174@gmail.com>
2024-04-22 15:09:43 +0900

    add color effect

--- cat Project/cat Project/start.c
+++ cat Project/cat Project/start.c
@@ -3,27 +3,75 @@
 #include <string.h>
 #include <stdlib.h>
 #include <conio.h>
-//#include<catFunc.h>
 
-//#pragma once
-//
-//#define pause system("pause > nul")		// DEFINE 선언
-//
-//#define UP 0
-//#define DOWN 1
-//#define SUBMIT 2
-//
+#pragma once
+
+#define pause system("pause > nul")		// DEFINE 선언
+
+#define UP 0
+#define DOWN 1
+#define SUBMIT 2
+
+#define color 9
+#define default 15
+ 
 void setconsleview() {
 	system("mode con:cols=80 lines=30");
 }
-//
-//int main(void) {
-//	CursorView(0);	// 커서의 깜빡임을 숨겨준다
-//	system("COLOR OF");	// 화면 색깔을 검정, 글씨 색깔을 하얀색으로 설정
-//	//mainPtr;
-//	pause;
-//	return 0;
-//}
+void gotoxy(int x, int y) {
+	HANDLE consolehandle = GetStdHandle(STD_OUTPUT_HANDLE);
+	COORD pos = { x, y };
+	SetConsoleCursorPosition(consolehandle, pos);
+}
+
+int keyControl() {
+	
+}
+
+int drawmenu() {
+	int x = 65, y = 18;
+	gotoxy(x, y);
+
+	while (1) {
+
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), default);
+		gotoxy(x, 18);
+		printf("시작하기\n");
+		gotoxy(x, 20);
+		printf("게임종료\n");
+
+		
+			if (y == 18) {
+				SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
+				gotoxy(x, 18);
+				printf("시작하기\n");
+				
+			}
+		    else{
+				SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), default);
+				gotoxy(x, 18);
+				printf("시작하기\n");
+				SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
+				gotoxy(x, 20);
+				printf("게임종료\n");
+			}
+		char ch;
+
+		ch = _getch();
+
+
+			switch (ch) {
+			case 72:
+				if (y > 18)y--;
+				break;
+			case 80:
+				if (y < 20) y++;
+				break;
+			}
+		
+		
+	}
+}
 int main() {
 	setconsleview();
 	
@@ -44,14 +92,17 @@ int main() {
 	printf("■        '       *    )==* (`    `) ~(                             *         □\n");
 	printf("□  *                 /     |     /    |          *                           ■\n");
 	printf("■                   /      |     ) ~  (                                      □\n");
-	printf("□                  /       |    /    ~ |                   ---------------   ■\n");
-	printf("■             *   |        /   |~   ~  |                  |    시작하기   |  □\n");
-	printf("□    __________/|_|__    _/_/|_|__~____/_/|_/|_/|_/|_/|_   ---------------   ■\n");
-	printf("■    |  |  |  |  |  | ) ) |  |  | ((  |  |  |  |  |  |     ---------------   □\n");
-	printf("□    |  |  |  |  |  | )_) |  |  |  )) |  |  |  |  |  |    |    게임종료   |  ■\n");
-	printf("■    |  |  |  |  |  |  |  |  |  | (/  |  |  |  |  |  |     ---------------   □\n");
+	printf("□                  /       |    /    ~ |                                     ■\n");
+	printf("■             *   |        /   |~   ~  |                                     □\n");
+	printf("□    __________/|_|__    _/_/|_|__~____/_/|_/|_/|_/|_/|_                     ■\n");
+	printf("■    |  |  |  |  |  | ) ) |  |  | ((  |  |  |  |  |  |                       □\n");
+	printf("□    |  |  |  |  |  | )_) |  |  |  )) |  |  |  |  |  |                       ■\n");
+	printf("■    |  |  |  |  |  |  |  |  |  | (/  |  |  |  |  |  |                       □\n");
 	printf("□                                                                            ■\n");
 	printf("■                                                                            □\n");
 	printf("□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■\n");
 	
+	drawmenu();
+
 }
+

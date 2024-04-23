04482e8d58684485bd6ed26c39fdd1c32a0b9285
ankm24 <ankm174@gmail.com>
2024-04-23 15:05:25 +0900

    change printf to slowPrint in story

--- cat Project/cat Project/start.c
+++ cat Project/cat Project/start.c
@@ -21,33 +21,41 @@ void gotoxy(int x, int y) {
 	COORD pos = { x, y };
 	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
 }
+void slowPrint(unsigned long speed, const char* s) { //타자 효과 함수
+	int i = 0;
+	while (s[i] != 0) {
+		printf("%c", s[i++]);
+		fflush(stdout);
+		Sleep(speed);
+	}
+}
 
 int story() {
 	system("cls");
 	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), default);
 	printf("■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□\n");
 	printf("□                                                                            ■\n");
-	printf("■   오늘도 지긋지긋한 아르바이트를 하러 간다...                              □\n");
+	slowPrint(1, "■      오늘도 지긋지긋한 아르바이트를 하러 간다...                           □\n");
 	printf("□                                                                            ■\n");
-	printf("■   매일매일이 똑같은 갑갑한 일상의 반복..                                   □\n");
+	slowPrint(1, "■      매일매일이 똑같은 갑갑한 일상의 반복..                                □\n");
 	printf("□                                                                            ■\n");
-	printf("■   지루하다. 평생을 이렇게 살아가는걸까?                                    □\n");
+	slowPrint(1, "■      지루하다.평생을 이렇게 살아가는걸까 ?                                 □\n");
 	printf("□                                                                            ■\n");
-	printf("■   ... 어, 저게 뭐지?                                                       □\n");
+	slowPrint(1, "■      ... 어, 저게 뭐지?                                                    □\n");
 	printf("□                                                                            ■\n");
-	printf("■   (자세히 살펴본다)                                                        □\n");
+	slowPrint(1, "■      (자세히 살펴본다)                                                     □\n");
 	printf("□                                                                            ■\n");
 	printf("■     /|____/|                                                               □\n");
 	printf("□   =( °w ° )=                                                             ■\n");
 	printf("■     )      (  //                                                           □\n");
 	printf("□    ( __ __  )//                                                            ■\n");
 	printf("■                                                                            □\n");
-	printf("□  -> 고양이가 짠~~!!!                                                       ■\n");
+	slowPrint(1, "□-> 고양이가 짠~~!!!                                                         ■\n");
 	printf("■                                                                            □\n");
-	printf("□   못 보던 고양이다. 귀여워~ 어떻게 할까?                                   ■\n");
+	slowPrint(1, "□   못 보던 고양이다. 귀여워~ 어떻게 할까?                                   ■\n");
 	printf("■                                                                            □\n");
 	printf("□                                                                            ■\n");
-	printf("■                            Enter를 눌러 다음으로 넘어가기 (DAY 1) >>>>>    □\n");
+	slowPrint(1, "■                            Enter를 눌러 다음으로 넘어가기 (DAY 1) >>>>>    □\n");
 	printf("□                                                                            ■\n");
 	printf("■                                                                            □\n");
 	printf("□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■\n");
@@ -104,9 +112,9 @@ int startScreen(void) {
 
 	printf("■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□\n");
 	printf("□                                                                            ■\n");
-	printf("■                 _   _  _ _|_   _ _ _|_   _  _ _  . _  __|_                 □\n");
-	printf("□                 _\VV(/_(/_ |   (_(_| |   |_)| (_) |(/_(_ |           '      ■\n");
-	printf("■                                         |       L|                         □\n");
+	printf("■                 _  _  _ _|_   _ _ _|_   _  _ _  . _  __|_                  □\n");
+	printf("□                 _VV(/_(/_ |   (_(_| |   |_)| (_) |(/_(_ |             *    ■\n");
+	printf("■                                        |       L|                          □\n");
 	printf("□    '      *         *          ,MMM8&&&&.                     *            ■\n");
 	printf("■                          *     MMM88&&&&&                                  □\n");
 	printf("□                                MMMM88&&&&    .    *                        ■\n");

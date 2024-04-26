9fbc2db642e0c71c78be1b5e5113bdb3e21bfd1a
starfall - miji0131 <dlalwl0131@naver.com>
2024-04-26 11:28:07 +0900

    화면 조절하고 싶다

--- cat Project/cat Project/miniGame.c
+++ cat Project/cat Project/miniGame.c
@@ -9,15 +9,25 @@
 #define HEIGHT 20 //높이
 #define STAR '@'
 #define PLAYER '^'
+#define GAME_DURATION 60  // 게임 시간(초)
 
 int playerX = WIDTH / 2;
 int starX = 0;
 int starY = 0;
 int score = 0;
 
+time_t startTime;
+
 
 void drawBoard() {
     system("cls");
+
+    // 게임의 남은 시간 계산
+    time_t currentTime = time(NULL);
+    int remainingTime = GAME_DURATION - (int)difftime(currentTime, startTime);
+    printf("남은 시간: %d\n", remainingTime);
+
+
     for (int y = 0; y < HEIGHT; y++) {
         for (int x = 0; x < WIDTH; x++) {
             if (y == HEIGHT - 1 && x == playerX)
@@ -80,10 +90,46 @@ int miniGame() {
     system("cls");
     catArt();
     drawBoard();
+
+    startTime = time(NULL);     // 게임 시작 시간 초기화
+
     while (1) {
+        // 현재 시간 갱신
+        time_t currentTime = time(NULL);
+        // 게임이 지정된 시간 이상 지났는지 확인
+        if (difftime(currentTime, startTime) >= GAME_DURATION)
+            break;      // 시간 종료
         drawBoard();
         updateStar();
         move();
+       
     }
+
+    // 게임 종료 후 게임 오버 메시지 출력
+    system("cls");
+    int x = 17, y = 5;
+    gotoxy(x, y);
+    printf(" ▣▣▣      ▣▣▣     ▣    ▣   ▣▣▣▣\n");
+    gotoxy(x, y + 1);
+    printf("▣     ▣  ▣      ▣  ▣ ▣▣ ▣  ▣\n");
+    gotoxy(x, y + 2);
+    printf("▣         ▣▣▣▣▣  ▣  ▣  ▣  ▣▣▣\n"); 
+    gotoxy(x, y + 3);
+    printf("▣    ▣▣ ▣      ▣  ▣  ▣  ▣  ▣\n"); 
+    gotoxy(x, y + 4); 
+    printf(" ▣▣▣▣  ▣      ▣  ▣      ▣  ▣▣▣▣\n");
+    gotoxy(x + 1, y + 6);
+    printf("  ■■■   ■      ■ ■■■■  ■■■■\n");
+    gotoxy(x + 1, y + 7);
+    printf(" ■     ■ ■      ■ ■        ■      ■\n");
+    gotoxy(x + 1, y + 8);
+    printf(" ■     ■  ■    ■  ■■■    ■■■■\n");
+    gotoxy(x + 1, y + 9);
+    printf(" ■     ■   ■  ■   ■        ■      ■\n");
+    gotoxy(x + 1, y + 10);
+    printf("  ■■■       ■     ■■■■  ■      ■\n\n");
+    gotoxy(x + 16, y + 15);
+    printf("YOUR SCORE: %d\n", score);
+    
     return 0;
 }

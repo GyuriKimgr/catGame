fb1875a1b2d256e5b1106900bf34f41b836a5a17
starfall - miji0131 <dlalwl0131@naver.com>
2024-04-29 12:33:07 +0900

    .

--- cat Project/cat Project/miniGame.c
+++ cat Project/cat Project/miniGame.c
@@ -15,36 +15,53 @@
 #define MAPX 3
 #define MAPY 2
 int starX = 0;
-int starY = HEIGHT / 3;
+int starY = 0;
 int score = 0;
 int x = 0;
 int y = 0;
 
 time_t startTime;
-
-
-void drawBoard() {
-    system("cls");
-
-    // 게임의 남은 시간 계산
+void timeCnt() {
     time_t currentTime = time(NULL);
     int remainingTime = GAME_DURATION - (int)difftime(currentTime, startTime);
     printf("남은 시간: %d\n", remainingTime);
-
-
-    for (int y = 0; y < HEIGHT; y++) {
-        for (int x = 0; x < WIDTH; x++) {
-            if (y == HEIGHT - 1 && x == playerX)
-                putchar(PLAYER);
-            else if (y == starY && x == starX)
-                putchar(STAR);
-            else
-                putchar(' ');
-        }
-        putchar('\n');
-    }
-    printf("Score: %d\n", score);
 }
+void playBoard() {
+    system("cls");
+    timeCnt();
+
+    gotoxy(20, 10);
+    printf("■■■■■■■■■■■■■■■■■■\n");
+    gotoxy(20, 11);
+    printf("■                                ■\n");
+    gotoxy(20, 12);
+    printf("■                                ■\n");
+    gotoxy(20, 13);
+    printf("■                                ■\n");
+    gotoxy(20, 14);
+    printf("■                                ■\n");
+    gotoxy(20, 15);
+    printf("■                                ■\n");
+    gotoxy(20, 16);
+    printf("■                                ■\n");
+    gotoxy(20, 17);
+    printf("■                                ■\n");
+    gotoxy(20, 18);
+    printf("■                                ■\n");
+    gotoxy(20, 19);
+    printf("■                                ■\n");
+    gotoxy(20, 20);
+    printf("■                                ■\n");
+    gotoxy(20, 21);
+    printf("■                                ■\n");
+    gotoxy(20, 22);
+    printf("■                                ■\n");
+    gotoxy(20, 23);
+    printf("■                                ■\n");
+    gotoxy(20, 24);
+    printf("■                                ■\n");
+    gotoxy(20, 25);
+    printf("■■■■■■■■■■■■■■■■■■\n");
 
     int playerX = 22, playerY = 22;
     starX = (rand() % (30)) + 22;
@@ -65,11 +82,16 @@ void drawBoard() {
             starX = (rand() % (30)) + 22; // 새로운 별의 가로 위치
             starY = 11; // 별의 세로 위치 초기화
         }
-        starX = (rand() % (WIDTH / 10)) * 10;
-        starY = 0;
-    }
-    Sleep(100);
-}
+        // 별이 아래로 떨어지는 로직
+        if (starY < 24) {
+            Sleep(100);
+            gotoxy(starX, starY);
+            printf("   "); // 이전 위치의 별 지우기
+            starY++; // 아래로 이동
+            gotoxy(starX, starY);
+            printf("★"); // 새로운 위치에 별 출력
+        }
+        else {
 
             gotoxy(starX, starY);
             printf("   \n");
@@ -123,8 +145,81 @@ void drawBoard() {
                 break;
             }
         }
+        // 현재 시간 갱신
+        time_t currentTime = time(NULL);
+        // 게임이 지정된 시간 이상 지났는지 확인
+        if (difftime(currentTime, startTime) >= GAME_DURATION) {
+            gotoxy(0, 0);
+            printf("남은 시간: 0\n");
+            break; // 시간 종료
+        }
+        else {
+            gotoxy(0, 0);
+            printf("남은 시간: %d\n", GAME_DURATION - (int)difftime(currentTime, startTime));
+        }
     }
 }
+
+void updateStar() {
+    starY++;
+    if (starY == y - 1) {
+        if (starX == x) {
+            score++;
+        }
+        starX = (rand() % (x / 10)) * 10;
+        starY = 0;
+    }
+    Sleep(100);
+}
+
+
+
+int miniGame() {
+    system("cls");
+    startTime = time(NULL);     // 게임 시작 시간 초기화
+    while (1) {
+        // 현재 시간 갱신
+        time_t currentTime = time(NULL);
+        // 게임이 지정된 시간 이상 지났는지 확인
+        if (difftime(currentTime, startTime) >= GAME_DURATION)
+            break;      // 시간 종료
+        timeCnt();
+        playBoard();
+    }
+   
+
+}
+
+//    // 게임 종료 후 게임 오버 메시지 출력
+//    system("cls");
+//    int x = 17, y = 5;
+//    gotoxy(x, y);
+//    printf(" ▣▣▣      ▣▣▣     ▣    ▣   ▣▣▣▣\n");
+//    gotoxy(x, y + 1);
+//    printf("▣     ▣  ▣      ▣  ▣ ▣▣ ▣  ▣\n");
+//    gotoxy(x, y + 2);
+//    printf("▣         ▣▣▣▣▣  ▣  ▣  ▣  ▣▣▣\n"); 
+//    gotoxy(x, y + 3);
+//    printf("▣    ▣▣ ▣      ▣  ▣  ▣  ▣  ▣\n"); 
+//    gotoxy(x, y + 4); 
+//    printf(" ▣▣▣▣  ▣      ▣  ▣      ▣  ▣▣▣▣\n");
+//    gotoxy(x + 1, y + 6);
+//    printf("  ■■■   ■      ■ ■■■■  ■■■■\n");
+//    gotoxy(x + 1, y + 7);
+//    printf(" ■     ■ ■      ■ ■        ■      ■\n");
+//    gotoxy(x + 1, y + 8);
+//    printf(" ■     ■  ■    ■  ■■■    ■■■■\n");
+//    gotoxy(x + 1, y + 9);
+//    printf(" ■     ■   ■  ■   ■        ■      ■\n");
+//    gotoxy(x + 1, y + 10);
+//    printf("  ■■■       ■     ■■■■  ■      ■\n\n");
+//    gotoxy(x + 16, y + 15);
+//    printf("YOUR SCORE: %d\n", score);
+//    
+//    return 0;
+//}
+
+
 //void drawBoard() {
 //    system("cls");
 //    // 게임의 남은 시간 계산
@@ -144,17 +239,7 @@ void drawBoard() {
 //        putchar('\n');
 //    }
 //}
-void updateStar() {
-    starY++;
-    if (starY == y - 1) {
-        if (starX == x) {
-            score++;
-        }
-        starX = (rand() % (x / 10)) * 10;
-        starY = 0;
-    }
-    Sleep(100);
-}
+
 
 //void move() {
 //  
@@ -167,47 +252,3 @@ void updateStar() {
 //    }
 //    Sleep(1);
 //}
-
-
-int miniGame() {
-
-    system("cls");
-    startTime = time(NULL);     // 게임 시작 시간 초기화
-    while (1) {
-        // 현재 시간 갱신
-        time_t currentTime = time(NULL);
-        // 게임이 지정된 시간 이상 지났는지 확인
-        if (difftime(currentTime, startTime) >= GAME_DURATION)
-            break;      // 시간 종료
-        timeCnt();
-        playBoard();
-    }
-
-    // 게임 종료 후 게임 오버 메시지 출력
-    system("cls");
-    int x = 17, y = 5;
-    gotoxy(x, y);
-    printf(" ▣▣▣      ▣▣▣     ▣    ▣   ▣▣▣▣\n");
-    gotoxy(x, y + 1);
-    printf("▣     ▣  ▣      ▣  ▣ ▣▣ ▣  ▣\n");
-    gotoxy(x, y + 2);
-    printf("▣         ▣▣▣▣▣  ▣  ▣  ▣  ▣▣▣\n"); 
-    gotoxy(x, y + 3);
-    printf("▣    ▣▣ ▣      ▣  ▣  ▣  ▣  ▣\n"); 
-    gotoxy(x, y + 4); 
-    printf(" ▣▣▣▣  ▣      ▣  ▣      ▣  ▣▣▣▣\n");
-    gotoxy(x + 1, y + 6);
-    printf("  ■■■   ■      ■ ■■■■  ■■■■\n");
-    gotoxy(x + 1, y + 7);
-    printf(" ■     ■ ■      ■ ■        ■      ■\n");
-    gotoxy(x + 1, y + 8);
-    printf(" ■     ■  ■    ■  ■■■    ■■■■\n");
-    gotoxy(x + 1, y + 9);
-    printf(" ■     ■   ■  ■   ■        ■      ■\n");
-    gotoxy(x + 1, y + 10);
-    printf("  ■■■       ■     ■■■■  ■      ■\n\n");
-    gotoxy(x + 16, y + 15);
-    printf("YOUR SCORE: %d\n", score);
-    
-    return 0;
-}
